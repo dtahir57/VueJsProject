696958fd122072b93fec1e5e7b1fc948751f3d90
dtahir57 <dtahir57@gmail.com>
2018-04-08 21:56:25 +0500

    This is VueJs , Express JS based Project

---
+++ src/components/Backend/Auth/Register.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="register">
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="Email" v-model="email" />
+    <input type="password" name="password" placeholder="Password" v-model="password" />
+    <button type="submit" @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import Auth from '@/services/auth'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register() {
+      const response = await Auth.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
